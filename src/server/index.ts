8d5ece57ce7079ca7cd4aa6e8062beb626b3b329
René van Asperen <rvanasperen@gmail.com>
2025-11-19 12:46:20 +0100

    Remove unused server variable and simplify code structure.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,11 @@
 import { file, serve } from "bun";
 
-const server = serve({
+serve({
     port: 8000,
     routes: {
         "/api": () => new Response("Api"),
     },
-    async fetch(req: Request): Promise<Response> {
+    async fetch(req) {
         const url = new URL(req.url);
 
         if (url.pathname === "/") {
@@ -22,5 +22,3 @@ const server = serve({
         return new Response('Not found', { status: 404 });
     },
 });
-
-console.log(`Listening on ${server.url}`);
